7e057ba34c17c466a4e9978138a66e9b979fb5a6
husnain417 <128898587+husnain417@users.noreply.github.com>
2025-05-05 19:59:51 +0500

    Update env.js

--- utils/env.js
+++ utils/env.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../.env' });
 
 const dbUrl = process.env.DB_URL;
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 module.exports = {
   dbUrl,
